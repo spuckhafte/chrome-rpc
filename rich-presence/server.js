b604e432f0fb1b2b72894c68123b8c52d4e6b2ae
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-03-20 23:30:20 +0530

    ::added settings.json:: => made changes accordingly

--- rich-presence/server.js
+++ rich-presence/server.js
@@ -5,19 +5,32 @@ const DiscordRPC = require('discord-rpc');
 const RPC = new DiscordRPC.Client({ transport: 'ipc' });
 DiscordRPC.register(clientId);
 
+// load json file from current directory
+const fs = require('fs');
+const file = fs.readFileSync('./settings.json');
+const settings = JSON.parse(file);
+
+const bannedSites = settings.bannedSites;
+const extraSites = settings.extraSites;
+const githubUsername = settings.githubUsername;
+const workingRepo = settings.workingRepo;
+let noUrl = settings.noUrl;
+
+noUrl = noUrl === 'rickroll' || noUrl === '' ? 'https://bit.ly/3CSPJO2' : noUrl;
+
 try {
     wss.on('connection', socket => {
         socket.onmessage = async ({ data }) => {
             if (data === 'No tab') {
                 RPC.removeAllListeners();
                 RPC.on('ready', async () => {
-                    setActivity('no chrome', 'Out of Chrome', 'https://bit.ly/3CSPJO2', 'None', 'nochrome');
+                    setActivity('no chrome', 'Out of Chrome', noUrl, 'None', 'nochrome');
                 });
                 RPC.login({ clientId }).catch(err => console.error(err));
             } else {
                 data = typeof data === 'string' ? JSON.parse(data) : data;
                 if (data.url.length >= 512) data.url = data.url.slice(0, 512);
-                if (data.url.includes('youtube.com')) {
+                if (data.url.includes('youtube.com') && !bannedSites.includes('youtube.com')) {
                     if (data.url.includes('watch?')) {
                         RPC.removeAllListeners();
                         RPC.on('ready', async () => {
@@ -39,7 +52,7 @@ try {
                         });
                         RPC.login({ clientId }).catch(err => console.error(err));
                     }
-                } else if (data.url.includes('stackoverflow.com') || data.url.includes('w3schools.com') || data.url.includes('codepen.io') || data.url.includes('geeksforgeeks.org') || data.url.includes('quora.com')) {
+                } else if ((data.url.includes('stackoverflow.com') || data.url.includes('w3schools.com') || data.url.includes('codepen.io') || data.url.includes('geeksforgeeks.org') || data.url.includes('quora.com')) && (!bannedSites.includes('stackoverflow.com') || !bannedSites.includes('w3schools.com') || !bannedSites.includes('codepen.io') || !bannedSites.includes('geeksforgeeks.org') || !bannedSites.includes('quora.com'))) {
                     let thumbnail = ''
                     if (data.url.includes('stackoverflow.com')) thumbnail = 'stack'
                     if (data.url.includes('w3schools.com')) thumbnail = 'w3s'
@@ -51,49 +64,49 @@ try {
                         setActivity('finding answers', data.title, data.url, 'Search', thumbnail);
                     });
                     RPC.login({ clientId }).catch(err => console.error(err));
-                } else if (data.url.includes('github.com')) {
+                } else if (data.url.includes('github.com') && !bannedSites.includes('github.com')) {
                     RPC.removeAllListeners();
                     RPC.on('ready', async () => {
                         setActivity('surfing', data.title, data.url, 'Search', 'github');
                     });
                     RPC.login({ clientId }).catch(err => console.error(err));
-                } else if (data.url.includes('reddit.com')) {
+                } else if (data.url.includes('reddit.com') && !bannedSites.includes('reddit.com')) {
                     RPC.removeAllListeners();
                     RPC.on('ready', async () => {
                         setActivity('memeing', data.title, data.url, 'Meme', 'reddit');
                     });
                     RPC.login({ clientId }).catch(err => console.error(err));
-                } else if (data.url.includes('twitter.com')) {
+                } else if (data.url.includes('twitter.com') && !bannedSites.includes('twitter.com')) {
                     RPC.removeAllListeners();
                     RPC.on('ready', async () => {
                         setActivity('tweeting', data.title, data.url, 'Tweet', 'twitter');
                     });
                     RPC.login({ clientId }).catch(err => console.error(err));
-                } else if (data.url.includes('discord.gg') || data.url.includes('discord.com')) {
+                } else if ((data.url.includes('discord.gg') || data.url.includes('discord.com')) && (!bannedSites.includes('discord.gg') || !bannedSites.includes('discord.com'))) {
                     RPC.removeAllListeners();
                     RPC.on('ready', async () => {
                         setActivity('discord', data.title, data.url, 'Search', 'discord');
                     });
                     RPC.login({ clientId }).catch(err => console.error(err));
-                } else if (data.url.includes('google.com')) {
+                } else if (data.url.includes('google.com') && !bannedSites.includes('google.com')) {
                     RPC.removeAllListeners();
                     RPC.on('ready', async () => {
                         setActivity('searching', data.title, data.url, 'Search', 'google');
                     });
                     RPC.login({ clientId }).catch(err => console.error(err));
-                } else if (data.url.includes('facebook.com')) {
+                } else if (data.url.includes('facebook.com') && !bannedSites.includes('facebook.com')) {
                     RPC.removeAllListeners();
                     RPC.on('ready', async () => {
                         setActivity('facebook', data.title, data.url, 'Go', 'facebook');
                     });
                     RPC.login({ clientId }).catch(err => console.error(err));
-                } else if (data.url.includes('instagram.com')) {
+                } else if (data.url.includes('instagram.com') && !bannedSites.includes('instagram.com')) {
                     RPC.removeAllListeners();
                     RPC.on('ready', async () => {
                         setActivity('instagram', data.title, data.url, 'Go', 'instagram');
                     });
                     RPC.login({ clientId }).catch(err => console.error(err));
-                } else if (data.url.includes('wikipedia.org')) {
+                } else if (data.url.includes('wikipedia.org') && !bannedSites.includes('wikipedia.org')) {
                     RPC.removeAllListeners();
                     RPC.on('ready', async () => {
                         setActivity('reading', data.title, data.url, 'Read', 'wiki');
@@ -109,15 +122,28 @@ try {
                     } else if (data.url.includes('127.0.0.1')) {
                         RPC.removeAllListeners();
                         RPC.on('ready', async () => {
-                            setActivity('editing', data.title, 'https://github.com/spuckhafte?tab=repositories', 'Repos', 'code');
+                            setActivity('editing', data.title, `https://github.com/${githubUsername}${workingRepo}`, 'Repos', 'code');
                         });
                         RPC.login({ clientId }).catch(err => console.error(err));
                     } else {
-                        RPC.removeAllListeners();
-                        RPC.on('ready', async () => {
-                            setActivity('unreachable site', 'Somewhere in Chrome', 'https://bit.ly/3CSPJO2', 'None', 'chrome');
-                        });
-                        RPC.login({ clientId }).catch(err => console.error(err));
+                        let extraSiteIncludes = false;
+                        for (let i = 0; i < extraSites.length; i++) {
+                            if (data.url.includes(extraSites[i])) {
+                                extraSiteIncludes = true;
+                                RPC.removeAllListeners();
+                                RPC.on('ready', async () => {
+                                    setActivity('surfing', data.title, data.url, 'Search', 'chrome');
+                                });
+                                RPC.login({ clientId }).catch(err => console.error(err));
+                            }
+                        }
+                        if (!extraSiteIncludes) {
+                            RPC.removeAllListeners();
+                            RPC.on('ready', async () => {
+                                setActivity('unreachable site', 'Somewhere in Chrome', noUrl, 'None', 'chrome');
+                            });
+                            RPC.login({ clientId }).catch(err => console.error(err));
+                        }
                     }
                 }
             }
